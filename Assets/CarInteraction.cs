d14d1e4742a2388aab4a27469ffe21416f9879c9
LZDragon <lazydragon193@gmail.com>
2024-02-13 20:33:16 -0500

    Added invalid input feedback

--- Assets/CarInteraction.cs
+++ Assets/CarInteraction.cs
@@ -10,6 +10,8 @@ public class CarInteraction : MonoBehaviour
     [SerializeField] private TMP_InputField makeInputField;
     [SerializeField] private TMP_Text carDisplayText;
     [SerializeField] private Button submitButton;
+    [SerializeField] private GameObject inputUI;
+    [SerializeField] private TMP_Text errorText;
     private bool carExists;
     private Car playerCar;
     
@@ -41,13 +43,35 @@ public class CarInteraction : MonoBehaviour
 
     void DisplayUserPrompt()
     {
+        inputUI.SetActive(true);
         submitButton.onClick.AddListener(OnSubmitButtonClick);
     }
 
     void OnSubmitButtonClick()
     {
-        
-        CreateCar(int.Parse(yearInputField.text), makeInputField.text);
+        int year = int.Parse(yearInputField.text);
+        string make = makeInputField.text;
+        if (year <= 2024 && year >= 1886)
+        {
+            if (make != "")
+            {
+                playerCar = new Car(year, make);
+                Debug.Log("Car Made");
+                carExists = true;
+                inputUI.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Make field is empty");
+                errorText.text = "Make field is empty";
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid year");
+            errorText.text = "Invalid year: year must be between 1886 and 2024";
+            yearInputField.text = "";
+        }
     }
 
     void CreateCar(int year, string make)
